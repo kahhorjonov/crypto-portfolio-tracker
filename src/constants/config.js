0306f03548f9334bce88fb802e290067bdd38efb
kahhorjonov <akbarjon3028@gmail.com>
2025-03-28 14:12:23 +0500

    env variables edited

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,5 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  process.env.REACT_APP_API_BASE_URL_LOCAL ||
   "http://localhost:4000";
 export const WEBSOCKET_URL =
   process.env.REACT_APP_WEBSOCKET_URL || "ws://localhost:4000";
